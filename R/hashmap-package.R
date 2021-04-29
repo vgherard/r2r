b895efbc91efbc1889a3b3b02af4c37128216fab
vgherard <vgherard@sissa.it>
2021-04-29 17:07:23 +0200

    import digest

--- R/hashmap-package.R
+++ R/hashmap-package.R
@@ -1,6 +1,8 @@
 #' @keywords internal
 "_PACKAGE"
 
+#' @importFrom digest digest
+
 # The following block is used by usethis to automatically manage
 # roxygen namespace tags. Modify with care!
 ## usethis namespace: start
