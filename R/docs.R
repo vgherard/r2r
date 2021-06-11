d473b473d0cd6f351e521afcd11cdf8c980eabe1
vgherard <vgherard@sissa.it>
2021-06-11 17:09:26 +0200

    General documentation

--- R/docs.R
+++ R/docs.R
@@ -9,21 +9,21 @@
 #' and \code{hashset}s provide an R implementation of
 #' \href{https://en.wikipedia.org/wiki/Hash_table}{hash tables}.
 #'
-#' See \link[r2r]{hashtable_methods} for the list of available methods for
-#' \code{hashmap} and \code{hashset} class objects.
+#' See \link[r2r]{hashtable_methods} for an overview of the available methods
+#' for \code{hashmap} and \code{hashset} class objects.
 #'
 #' @param ... these arguments can be used to specify a set of initial elements
 #' to be inserted in the \code{hashmap} or \code{hashset}. For \code{hashmap()},
 #' each of these should be a \code{list} of two elements (a key-value pair).
-#' @param hash the (string valued) hash function applied to keys. Required for
-#' advanced use only; see Details.
-#' @param compare the (boolean valued) comparison function used for testing key
-#' equality. Required for advanced use only; see Details.
-#' @param key_preproc key pre-processing function applied to keys before hashing
-#' and comparison. Required for advanced use only; see Details.
-#' @param throw \code{TRUE} or \code{FALSE}. If \code{TRUE}, an exception is
-#' thrown upon query of a missing key; otherwise, a default value (specified
-#' through the \code{default} argument) is returned.
+#' @param hash_fn the (string valued) hash function applied to keys.
+#' Required for advanced use only; see Details.
+#' @param compare_fn the (boolean valued) comparison function used for
+#' testing key equality. Required for advanced use only; see Details.
+#' @param key_preproc_fn key pre-processing function applied to keys before
+#' hashing and comparison. Required for advanced use only; see Details.
+#' @param on_missing_key either \code{"throw"} or \code{"default"}.
+#' In the second case, an exception is thrown upon query of a missing key; otherwise, a default value
+#' (specified through the \code{default} argument) is returned.
 #' @param default default value associated with missing keys. This will be
 #' returned only if \code{throw} is \code{FALSE}.
 #'
@@ -39,22 +39,23 @@
 #'
 #' By default, key equality is tested through \code{\link[base]{identical}()}.
 #' For some use cases, it may be sensible to employ a different comparison
-#' function, which can be assigned through the \code{compare} argument. In this
-#' case, one must also make sure that equal (in the sense of \code{compare()})
-#' keys get also assigned the same hashes by \code{hash()}. A simple way to
+#' function, which can be assigned through the \code{compare_fn} argument. In this
+#' case, one must also make sure that equal (in the sense of
+#' \code{compare_fn()})
+#' keys get also assigned the same hashes by \code{hash_fn()}. A simple way to
 #' ensure this is to use to use a key pre-processing function, to be applied
-#' before both key hashing \emph{and} comparison. The \code{key_preproc}
+#' before both key hashing \emph{and} comparison. The \code{key_preproc_fn}
 #' argument provides a short-cut to this, by automatically composing both the
-#' provided \code{hash()} and \code{compare()} functions with
-#' \code{key_preproc()} function. This is illustrated in an example below.
+#' provided \code{hash_fn()} and \code{compare_fn()} functions with
+#' \code{key_preproc_fn()} function. This is illustrated in an example below.
 #'
 #' One might also want to set set specific hash and/or key comparison functions
 #' for efficiency reasons, e.g. if the \code{default_hash_fn()} function produces
 #' many collisions between inequivalent keys.
 #'
-#' When \code{throw} is \code{TRUE}, querying a missing key will cause an error.
-#' In this case, an \link[rlang]{rlang} condition of class
-#' \code{"r2r_missing_key"} is returned, which can be useful for testing
+#' When \code{on_missing_key} is equalt to \code{"throw"}, querying a missing
+#' key will cause an error. In this case, an \link[rlang]{rlang} condition of
+#' class \code{"r2r_missing_key"} is returned, which can be useful for testing
 #' purposes.
 #'
 #' @examples
@@ -73,11 +74,30 @@
 #' @name hashtable
 NULL
 
-#' @title Methods for S3 classes \code{map} and \code{set}
+#' @title Methods for S3 classes \code{hashmap} and \code{hashset}
 #'
 #' @author Valerio Gherardi
+#' @md
+#' @description This page provides an overview of the available methods for
+#' \code{hashmap} and \code{hashset} objects. We list methods based on the
+#' general type of task addressed.
 #'
-#' @description
-#'
+#' ### Basic read/write operations
+#' - \code{\link[r2r]{insert}()}
+#' - \code{\link[r2r]{delete}()}
+#' - \code{\link[r2r]{query}()}
+#' - \link[r2r]{subsetting_hashtables}: \code{`[[`}, \code{`[[<-`}, \code{`[`}
+#' and \code{`[<-`}
+#' ### Size of hash table
+#' - \code{length()}
+#' ### Other key or value access operations
+#' - \code{\link{keys}()}
+#' - \code{\link{values}()}
+#' - \code{\link{has_key}}, \code{\link[r2r:has_key]{%has_key%}}
+#' ### Get/set hashtable properties
+#' - \code{\link{hash_fn}()}
+#' - \code{\link{compare_fn}()}
+#' - \code{\link{on_missing_key}()}
+#' - \code{\link{default}()}
 #' @name hashtable_methods
 NULL
